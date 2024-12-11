71834dc51f5f077362b79bd59b474f1e97c51aa0
Melvin0419 <melvin.hsieh1995@gmail.com>
2024-12-11 18:47:29 +0800

    adjust db create

--- website/__init__.py
+++ website/__init__.py
@@ -20,7 +20,7 @@ def create_app():
     from website import model
 
     with app.app_context():
-        db.create_all()
+        db.create_all(checkfirst=True)
 
     # Login manager
     login_manager = LoginManager()
